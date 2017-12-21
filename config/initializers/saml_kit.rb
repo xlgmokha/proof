10c32258551fc59da175d8ee53ff308da9599ef1
mokha <mokha@cisco.com>
2017-12-21 16:55:43 -0700

    return metadata if found.

--- config/initializers/saml_kit.rb
+++ config/initializers/saml_kit.rb
@@ -1,9 +1,14 @@
+$next_key_pair_index = 0
+
 class OnDemandRegistry
   def initialize(original)
     @original = original
   end
 
   def metadata_for(entity_id)
+    found = @original.metadata_for(entity_id)
+    return found if found
+
     @original.register_url(entity_id, verify_ssl: Rails.env.production?)
     @original.metadata_for(entity_id)
   end
