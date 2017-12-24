913910df67f8e5f8464a93a106a651976746aeb6
mo <mo.khan@gmail.com>
2017-12-23 22:38:16 -0700

    assert content type.

--- spec/requests/metadata_controller_spec.rb
+++ spec/requests/metadata_controller_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 describe MetadataController do
   describe "#show" do
-    it 'returns the metadata' do
-      get '/metadata'
+    before { get '/metadata' }
 
+    it 'returns the metadata' do
       expect(response).to have_http_status(:ok)
       metadata = Saml::Kit::Metadata.from(response.body)
       expect(metadata.entity_id).to eql(Saml::Kit.configuration.issuer)
+    end
+
+    it 'uses the correct content type' do
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq("application/samlmetadata+xml")
     end
   end
 end
