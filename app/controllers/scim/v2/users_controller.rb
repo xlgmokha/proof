721a221485d4a26ac2dc7638a33fabbcc1b959bc
mokha <mokha@cisco.com>
2018-01-25 15:59:07 -0700

    extract UserRepository class.

--- app/controllers/scim/v2/users_controller.rb
+++ app/controllers/scim/v2/users_controller.rb
@@ -41,26 +41,5 @@ module Scim
         UserRepository.new
       end
     end
-
-    class UserRepository
-      def find!(id)
-        User.find_by!(uuid: id)
-      end
-
-      def create!(params)
-        password = SecureRandom.hex(32)
-        User.create!(email: params[:userName], password: password)
-      end
-
-      def update!(id, params)
-        user = find!(id)
-        user.update!(email: params[:userName])
-        user
-      end
-
-      def destroy!(id)
-        find!(id).destroy!
-      end
-    end
   end
 end
