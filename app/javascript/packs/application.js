c3a1f19bf6a229c1da2c1319ede40cce1e4938c3
mokha <mokha@cisco.com>
2018-01-26 15:40:25 -0700

    hook up stimulus js.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,5 +7,11 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import "../../assets/stylesheets/application.css"
 console.log('Hello World from Webpacker')
+
+import { Application } from "stimulus"
+import { autoload } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const controllers = require.context("./controllers", true, /\.js$/)
+autoload(controllers, application)
