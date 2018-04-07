3821743547509b45b9ade141e6f3cfe84e3f255f
mo <mo.khan@gmail.com>
2018-04-07 13:03:41 -0600

    run webpack as part of cibuild.

--- app/javascript/controllers/tfa_controller.js
+++ app/javascript/controllers/tfa_controller.js
@@ -5,8 +5,8 @@ export default class extends Controller {
   get secret() { return this.targets.find("secret") }
 
   present() {
-    #let tfa = new Tfa(this.secret.value);
-    #console.log(tfa.qr_code());
+    //let tfa = new Tfa(this.secret.value);
+    //console.log(tfa.qr_code());
     console.log("CLICKED");
   }
 }
