6fc370720a281a922d8a8b16e1ceee63110971d4
mo <mo.khan@gmail.com>
2018-04-09 09:34:17 -0600

    export jquery.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+  })
+)
 
 module.exports = environment
