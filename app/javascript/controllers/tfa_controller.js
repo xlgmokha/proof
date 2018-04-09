6040a2dd2ae6997ce13e8f61588687fe74376676
mo <mo.khan@gmail.com>
2018-04-09 12:02:04 -0600

    move canvas element into div.

--- app/javascript/controllers/tfa_controller.js
+++ app/javascript/controllers/tfa_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   get secret() { return this.targets.find("secret") }
   get canvas() { return this.targets.find("canvas") }
 
-  present() {
+  connect() {
     QRCode.toCanvas(this.canvas, this.secret.value, (error) => {
       if (error) console.error(error);
     });
