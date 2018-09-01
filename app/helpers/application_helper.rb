c96909d2a4911616db3149354b2c9d8b6b1bc2be
mo <mo.khan@gmail.com>
2018-09-01 17:29:03 -0600

    display flash error messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def alert_class_for(type)
+    case type.to_sym
+    when :notice
+      'alert-success'
+    when :warning
+      'alert-warning'
+    when :error
+      'alert-danger'
+    else
+      'alert-primary'
+    end
+  end
 end
