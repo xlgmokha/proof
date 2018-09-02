663d95619ac27034113d436e996940f1b25ebb9b
mo <mo.khan@gmail.com>
2018-09-02 14:31:55 -0600

    fix lint error.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,7 +11,8 @@ class RegistrationsController < ApplicationController
     User.create!(user_params)
     redirect_to new_session_path
   rescue ActiveRecord::RecordInvalid => invalid
-    redirect_to new_registration_path, error: invalid.record.errors.full_messages
+    redirect_to new_registration_path,
+                error: invalid.record.errors.full_messages
   end
 
   private
