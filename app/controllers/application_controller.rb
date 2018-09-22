e2024665858ec7a436ce7a469d256e41c4be81e8
mokha <mokha@cisco.com>
2018-09-22 11:44:57 -0600

    Authentication -> Authenticatable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include Authentication
+  include Authenticatable
   protect_from_forgery with: :exception
   add_flash_types :error, :warning
 
--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module Authentication
+module Authenticatable
   extend ActiveSupport::Concern
   included do
     before_action :apply_current_request_details
