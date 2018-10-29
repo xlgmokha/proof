05442ae19a4acc575e136ad0281f6e64ec3f128b
mo <mo@mokhan.ca>
2018-10-29 09:08:00 -0600

    load JWT private key from env var.

--- config/initializers/configuration.rb
+++ config/initializers/configuration.rb
@@ -4,7 +4,11 @@ config = Rails.application.config
 config.generators do |g|
   g.orm :active_record, primary_key_type: :uuid
 end
-config.x.jwt.private_key = OpenSSL::PKey::RSA.new(2048)
+if ENV['JWT_PRIVATE_KEY'].present?
+  config.x.jwt.private_key = OpenSSL::PKey::RSA.new(ENV['JWT_PRIVATE_KEY'])
+else
+  config.x.jwt.private_key = OpenSSL::PKey::RSA.new(4096)
+end
 
 I18n.available_locales = [:en, :es, :fr, :ja, :ko]
 I18n.default_locale = :en
