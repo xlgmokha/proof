818df67ad878d1f84716a695b1b16e7c711aceeb
mo <mo@mokhan.ca>
2018-10-29 11:37:55 -0600

    load key pairs from env variables.

--- config/initializers/saml_kit.rb
+++ config/initializers/saml_kit.rb
@@ -1,18 +1,26 @@
 # frozen_string_literal: true
 
-class OnDemandRegistry < SimpleDelegator
+class OnDemandRegistry < Saml::Kit::DefaultRegistry
   def metadata_for(entity_id)
-    found = __getobj__.metadata_for(entity_id)
+    found = super(entity_id)
     return found if found
 
-    __getobj__.register_url(entity_id, verify_ssl: Rails.env.production?)
-    __getobj__.metadata_for(entity_id)
+    register_url(entity_id, verify_ssl: Rails.env.production?)
+    super(entity_id)
   end
 end
 
 Saml::Kit.configure do |x|
   x.entity_id = ENV['ISSUER']
-  x.registry = OnDemandRegistry.new(x.registry)
+  x.registry = OnDemandRegistry.new
   x.logger = Rails.logger
-  5.times { x.generate_key_pair_for(use: :signing) }
+  if ENV['SAML_PRIVATE_KEY'].present? && ENV['SAML_X509_CERTIFICATE'].present?
+    x.add_key_pair(
+      ENV['SAML_X509_CERTIFICATE'],
+      ENV['SAML_PRIVATE_KEY'],
+      use: :signing
+    )
+  else
+    5.times { x.generate_key_pair_for(use: :signing) }
+  end
 end
--- spec/documentation.rb
+++ spec/documentation.rb
@@ -9,22 +9,55 @@ require 'factory_bot_rails'
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.before :suite do
-    FileUtils.rm_rf(Rails.root.join('doc/_cassettes/'))
+    FileUtils.rm_rf(Rails.root.join('tmp/_cassettes/'))
     Net::Hippie.logger = Logger.new('/dev/null')
     VCR.configure do |x|
-      x.cassette_library_dir = "doc/_cassettes"
+      x.cassette_library_dir = "tmp/_cassettes"
       x.hook_into :webmock
     end
   end
 
   config.after :suite do
-    template = IO.read('doc/_includes/curl.erb')
-    erb = ERB.new(template)
-    Dir["doc/_cassettes/**/*.yml"].each do |cassette|
+    erb = ERB.new(IO.read('doc/_includes/curl.erb'))
+    Dir["tmp/_cassettes/**/*.yml"].each do |cassette|
       @configuration = YAML.safe_load(IO.read(cassette))
       result = erb.result(binding)
       IO.write("doc/_includes/#{File.basename(cassette).parameterize.gsub(/-yml/, '')}.html", result)
     end
+  end
+end
+
+class UserAgent
+  def login_with(scheme: 'https', host:, email:, password:, issuer:)
+    authn_request = Saml::Kit::AuthenticationRequest.build(configuration: Saml::Kit.configuration) do |x|
+      x.issuer = issuer
+      x.embed_signature = false
+    end
+    body = { SAMLRequest: Base64.strict_encode64(authn_request.to_xml) }
+    response = client.post("#{scheme}://#{host}/session/new", body: URI.encode_www_form(body))
+    form = Nokogiri::HTML(response.body).css('form').last
+    body.merge!(
+      'authenticity_token' => form.css('[name=authenticity_token]').first.attribute('value').value,
+      'user[email]' => email,
+      'user[password]' => password
+    )
+    session_cookie = parse_cookie(response['Set-Cookie'])
+    response = client.post("#{scheme}://#{host}/session", headers: { 'Cookie' => session_cookie }, body: URI.encode_www_form(body))
+
+    session_cookie = parse_cookie(response['Set-Cookie'])
+    response = client.get(response['Location'], headers: { 'Cookie' => session_cookie })
+    encoded_saml_response = Nokogiri::HTML(response.body).css('#SAMLResponse').attribute('value').value
+    Saml::Kit::Bindings::HttpPost.new(location: '').deserialize(SAMLResponse: encoded_saml_response)
+  end
+
+  private
+
+  def client
+    @client ||= Net::Hippie::Client.new(verify_mode: OpenSSL::SSL::VERIFY_NONE, headers: {})
+  end
+
+  def parse_cookie(value)
+    value.split(';')[0]
   end
 end
 
@@ -34,6 +67,7 @@ RSpec.describe "documentation" do
   let(:scheme) { ENV.fetch('SCHEME', 'https') }
   let(:client) { create(:client) }
   let(:user) { create(:user) }
+  let(:user_agent) { UserAgent.new }
 
   specify do
     VCR.use_cassette("get-well-known-oauth-authorization-server") do
@@ -71,11 +105,10 @@ RSpec.describe "documentation" do
   end
 
   specify do
-    headers = { 'Authorization' => ActionController::HttpAuthentication::Basic.encode_credentials(client.to_param, client.password) }
-    saml_request = instance_double(Saml::Kit::AuthenticationRequest, id: Xml::Kit::Id.generate, issuer: Saml::Kit.configuration.entity_id, trusted?: true)
-    saml = Saml::Kit::Assertion.build_xml(user, saml_request)
-    body = { grant_type: 'urn:ietf:params:oauth:grant-type:saml2-bearer', assertion: Base64.urlsafe_encode64(saml) }
     VCR.use_cassette("oauth-tokens-saml-assertion") do
+      saml = user_agent.login_with(scheme: scheme, host: host, email: user.email, password: user.password, issuer: 'https://saml-kit-airport.herokuapp.com/service_providers/73db6338-5d35-4271-812c-d4c6fbe45cca')
+      headers = { 'Authorization' => ActionController::HttpAuthentication::Basic.encode_credentials(client.to_param, client.password) }
+      body = { grant_type: 'urn:ietf:params:oauth:grant-type:saml2-bearer', assertion: Base64.urlsafe_encode64(saml.assertion.to_xml) }
       response = hippie.post("#{scheme}://#{host}/oauth/tokens", body: body, headers: headers)
       expect(response.code).to eql('200')
     end
