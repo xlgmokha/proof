b0a7d89029ddc3d1e7b6a6d6a445725a82e95418
mo <mo.khan@gmail.com>
2019-01-13 14:12:16 -0700

    load schema from config

--- app/models/scim/user_mapper.rb
+++ app/models/scim/user_mapper.rb
@@ -7,17 +7,17 @@ module SCIM
     end
 
     def map_from(user)
-      schema = SCIM::Schema.user
-      x = Scim::Kit::V2::Resource.new(schemas: [schema], location: @url_helpers.scim_v2_user_url(user))
-      x.meta.version = user.lock_version
-      x.meta.created = user.created_at
-      x.meta.last_modified = user.updated_at
-      x.id = user.id
-      x.user_name = user.email
-      x.locale = user.locale
-      x.timezone = user.timezone
-      x.emails = [{ value: user.email, primary: true }]
-      x
+      schema = Scim::Kit::V2.configuration.schemas[Scim::Kit::V2::Schemas::USER]
+      Scim::Kit::V2::Resource.new(schemas: [schema], location: @url_helpers.scim_v2_user_url(user)) do |x|
+        x.meta.version = user.lock_version
+        x.meta.created = user.created_at
+        x.meta.last_modified = user.updated_at
+        x.id = user.to_param
+        x.user_name = user.email
+        x.locale = user.locale
+        x.timezone = user.timezone
+        x.emails = [{ value: user.email, primary: true }]
+      end
     end
   end
 end
